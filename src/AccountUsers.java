c936587711cadbf12c3fffb25d44449c31158fe1
Sauatdev <zhasbolatov04@gmail.com>
2025-05-19 16:33:20 +0500

    add:new createAccount class

--- src/AccountUsers.java
+++ src/AccountUsers.java
@@ -2,8 +2,11 @@ import java.util.HashMap;
 
 //fixme: класстың атауы бас әріппен басталады
 //fixme 2: дұрыс емес демейм, бірақ неге осы классты статик қылдым деген сұраққа жауап бере алуың керек
-public static class accountUser {
+public class AccountUser {
 public HashMap<String, String> accounts = new HashMap<>();
+
+
+
 
 
 public void createAccount(String username, String password){
@@ -11,13 +14,14 @@ public void createAccount(String username, String password){
     System.out.println("Аккаунт құрылды");
 
 
-};
+}
 
 
 }
+        public void main() {
+            AccountUser user = new AccountUser();
+            user.createAccount("zhasbolatov@gmail.com","Sauat_2004");
+            System.out.println(user.accounts );
+        }
 
-public void main() {
-    accountUser user = new accountUser();
-    user.createAccount("zhasbolatov@gmail.com","Sauat_2004");
-    System.out.println(user.accounts );
-}
+
