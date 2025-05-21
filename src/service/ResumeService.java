25c981a604cbcc36b39531c142fd1e4fab7ebd9b
Sauatdev <zhasbolatov04@gmail.com>
2025-05-21 15:23:18 +0500

    add:new resumeService

---
+++ src/service/ResumeService.java
@@ -0,0 +1,6 @@
+package service;
+
+public interface ResumeService {
+    public void displayResume(String name, String surname );
+    public void updateresume();
+}
---
+++ src/service/impl/ResumeServiceImpl.java
@@ -0,0 +1,17 @@
+package service.impl;
+
+
+import service.ResumeService;
+
+
+public class ResumeServiceImpl implements ResumeService {
+    @Override
+    public void displayResume() {
+
+    }
+
+    @Override
+    public void updateresume() {
+
+    }
+}
